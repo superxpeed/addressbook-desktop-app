866072e549b189010285ee796da9e1eed7cfc274
Nikita Schneider <komesergey@gmail.com>
2023-02-18 02:23:40 +0300

    Building .dmg

--- forge.config.js
+++ forge.config.js
@@ -15,17 +15,11 @@ module.exports = {
             },
         },
         {
-            name: '@electron-forge/maker-zip',
-            platforms: ['darwin'],
-        },
-        {
-            name: '@electron-forge/maker-deb',
-            config: {},
-        },
-        {
-            name: '@electron-forge/maker-rpm',
-            config: {},
-        },
+            name: '@electron-forge/maker-dmg',
+            config: {
+                format: 'ULFO'
+            }
+        }
     ],
     plugins: [
         {
