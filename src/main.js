911200f28a8f50e3bbb0af58896f6697d82ee0c6
Nikita <komesergey@gmail.com>
2023-02-18 14:29:53 +0300

    Added splash screen

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -10,14 +9,20 @@ const createWindow = () => {
     width: 1400,
     height: 900,
     frame: true,
+    show: false,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
       webSecurity: false
     },
   });
-
+  let splash = new BrowserWindow({width: 800, height: 600, transparent: true, frame: false, alwaysOnTop: true});
+  splash.loadURL(`file://${__dirname}/splash.html`);
   mainWindow.setMenuBarVisibility(false);
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY + '#login');
+  mainWindow.once('ready-to-show', () => {
+    splash.destroy();
+    mainWindow.show();
+  });
 };
 
 app.on('ready', createWindow);
