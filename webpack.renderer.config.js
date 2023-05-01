58f97a0817c67d5e26972d3a4c70a1663869fba2
dredwardhyde <komesergey@gmail.com>
2023-05-01 03:15:23 +0300

    Refactoring

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,4 +1,5 @@
 const rules = require('./webpack.rules');
+const TerserPlugin = require('terser-webpack-plugin');
 
 rules.push({
     test: /\.css$/,
@@ -8,5 +9,16 @@ rules.push({
 module.exports = {
     module: {
         rules,
+    },
+    optimization: {
+        minimizer: [(compiler) => {
+            new TerserPlugin({
+                terserOptions: {
+                    compress: true,
+                    sourceMap: true,
+                    keep_fnames: true
+                }
+            }).apply(compiler);
+        },],
     },
 };
